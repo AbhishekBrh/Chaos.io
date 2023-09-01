0ada38e5ab0a983089579c428ca346918bef0a6b
AbhishekBrh <101127849+AbhishekBrh@users.noreply.github.com>
2023-09-01 21:54:32 +0530

    ChaosDrips

---
+++ script.js
@@ -0,0 +1,33 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+   bar.addEventListener('click', ()=> {
+     nav.classList.add('active');
+   })
+}
+
+if (close) {
+  close.addEventListener('click', ()=> {
+    nav.classList.remove('active');
+  })
+}
+
+
+        var mainimg = document.getElementById("mainimg");
+        var smallimg = document.getElementsByClassName("small-img");
+
+        smallimg[0].addEventListener("click", function() {
+        mainimg.src = smallimg[0].src;
+          });
+          smallimg[1].addEventListener("click", function() {
+  mainimg.src = smallimg[1].src;
+});
+smallimg[2].addEventListener("click", function() {
+  mainimg.src = smallimg[2].src;
+});
+smallimg[3].addEventListener("click", function() {
+  mainimg.src = smallimg[3].src;
+});
+      
